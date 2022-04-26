08b01f793c88424e7e906a3201e0f058cd0ca2ad
David <83718324+david-mov@users.noreply.github.com>
2022-04-25 21:03:42 -0300

    homework 2

---
+++ homework-2/main.py
@@ -0,0 +1,26 @@
+# 1. Escriba una declaración if que solicite el nombre del usuario a través de la función input(). 
+# Si el nombre es "Bond", escriba "Bienvenido a bordo 007". De lo contrario, imprima "Buenos días NOMBRE". 
+# (Reemplazar Nombre con el nombre del usuario)
+
+def requestName():
+    print()
+
+# 2. Muestra un menú con varias opciones y lee la variable 'opcion', imprime la opción seleccionada como 
+# 'Seleccionaste la opcion <opcion>'. Si la opción no está en las condiciones, imprime “Opción no válida”.
+
+# Ejemplo
+# Seleccione una opción
+# 1. Imprimir hola
+# 2. Muéstrame un héroe
+# 3. Dame la hora
+# 4. Salir
+
+def displayMenu():
+    print()
+
+# 3.Leer dos puntos (cuatro variables (una para cada punto) , 'x1' y 'y1').
+# Muestra cuál es el punto más cercano al origen. 
+# Úselo para determinar la distancia desde el primer punto hasta el origen. d = ( x1  2 + y1  2) ** 0.5
+
+def calculateDistance():
+    print()
